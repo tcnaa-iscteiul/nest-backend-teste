bcc9771256e87defdf3fae5554cdaa421591b359
tatiana-clim <43815866+tcnaa-iscteiul@users.noreply.github.com>
2022-08-24 00:37:02 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    /*
+    
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', '*');
         res.header('Access-Control-Allow-Headers', '*');
         next();
     });
-    
+    /*
     const allowCors = fn => async (req, res) => {
         res.setHeader('Access-Control-Allow-Credentials', true)
         res.setHeader('Access-Control-Allow-Origin', '*')
